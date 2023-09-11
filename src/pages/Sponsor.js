3f19a89a010e81ca6daa4d2c612ba255499b1458
Arunabha-NOTE <arunabham2017@gmail.com>
2023-09-11 22:46:20 +0530

    Update Sponsor.js
    sponsor page fixed

--- src/pages/Sponsor.js
+++ src/pages/Sponsor.js
@@ -1,15 +1,30 @@
 import React from 'react'
 import SponsorCard from "../components/SponsorCard";
 import Navbar from "../components/Navbar";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function Sponsor() {
+    AOS.init({
+        startEvent: 'DOMContentLoaded',
+        initClassName: 'aos-init',
+        animatedClassName: 'aos-animate',
+        debounceDelay: 50,
+        throttleDelay: 99,
+        offset: 120,
+        delay: 200,
+        duration: 800,
+        easing: 'ease',
+        anchorPlacement: 'top-bottom',
+
+    });
   return (
     <>
         <div className='bg-[#e3ebfa]'>
             <Navbar />
-            <div className='flex flex-col items-center justify-center'>
+            <div className='flex flex-col items-center justify-center' data-aos="fade-up">
                 <div>
-                    <h1 className='text-6xl text-center text-transparent bg-clip-text bg-gradient-to-tl from-blue-950 to-blue-500 font-extrabold mb-[5vh]'>
+                    <h1 className='text-6xl text-center font-[#5A6473] font-bold mb-[5vh]'>
                         SPONSORS
                     </h1>
                 </div>

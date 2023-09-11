e8e98973191fdca062ee222197e3e83840878c58
Pujan Modha <pujanmodha19@gmail.com>
2023-09-12 01:55:02 +0530

    minor changes in about page.

--- src/components/HomeCard.js
+++ src/components/HomeCard.js
@@ -29,7 +29,7 @@ const HomeCard = () => {
             {TracksData.map((Tracks, index) => (
                 <div key={index} className="items-stretch h-full align-middle">
                     <div className="items-stretch h-full align-middle">
-                        <div className="max-w-full rounded overflow-hidden shadow-lg items-stretch h-full align-middle">
+                        <div className="max-w-full rounded-xl overflow-hidden shadow items-stretch h-full align-middle">
                             <img className="w-full px-12 py-2" src={Tracks.TracksImage} alt="Sunset in the mountains"/>
                             <div className="px-6 py-4 align-middle">
                                 <div className="font-semibold text-center sm:text-left text-xl mb-2 align-middle">{Tracks.TracksName}</div>

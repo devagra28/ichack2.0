fbd7fb5540e727d9d95003ee9b472be449a7fa92
Pujan Modha <pujanmodha19@gmail.com>
2023-09-12 01:55:48 +0530

    minor changes in homepage.

--- src/components/HomeCard.js
+++ src/components/HomeCard.js
@@ -1,28 +1,6 @@
 import TracksData from "../assets/TracksData";
 import React from "react";
 
-// const HomeCard = () => {
-//     return(
-//         <>
-//             {TracksData.map = ((tracks, index) => (
-//                 <div key={index} className="mt-8">
-//                     <div>
-//                         <div className="max-w-sm rounded overflow-hidden shadow-lg">
-//                             <img className="w-full" src={tracks.placeholder} alt="Sunset in the mountains"/>
-//                             <div className="px-6 py-4">
-//                                 <div className="font-bold text-xl mb-2">{tracks.TracksName}</div>
-//                                 <p className="text-gray-700 text-base">
-//                                     {tracks.TracksAbout}
-//                                 </p>
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </div>
-//             ))}</>
-//
-//     )
-// }
-
 const HomeCard = () => {
     return (
         <>
